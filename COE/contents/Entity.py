a7017b83674a917500d28b75e0941e2dde014d85
Octave <octavegassot@hotmail.fr>
2021-10-12 00:05:43 +0200

    Creation of Entity Class

---
+++ COE/contents/Entity.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+"""
+    Contains the Class Entity, from which all content class inherit.
+"""
+
+from typing import List
+
+
+class Entity:
+    def __init__(self, name: str, hp: int, positions: tuple, height: int, width: int, line_of_sight: int, img):
+        self.name = name
+        self.positions = positions
+        self.height = height
+        self.width = width
+        self.line_of_sight = line_of_sight
+        self.img = img
