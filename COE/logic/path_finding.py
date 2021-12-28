6a00b4dd2c6e1b556cba2edea7dd7898d20d7c68
Quoc Thinh NGUYEN <quoc_thinh.nguyen@insa-cvl.fr>
2021-12-28 14:10:40 +0100

    changing AStar class to finding_path function+

--- COE/logic/path_finding.py
+++ COE/logic/path_finding.py
@@ -1,8 +1,6 @@
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
-from COE.contents.unit.enum.unit_types import UnitTypes
-from COE.map.map import Map
 
 """
 flow:
