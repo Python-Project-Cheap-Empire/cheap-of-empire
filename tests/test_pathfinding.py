abe92bfe808fe2cf97e1a2ea6cf0f30e4cd925a2
Quoc Thinh NGUYEN <90769805+QuocThinhNGUYEN-JONES@users.noreply.github.com>
2021-12-30 22:57:48 +0700

    first version of pathfinding (#37)
    * first version of pathfinding
    
    * adding tests for path_finding
    
    * fix test with tox
    
    * adding unit_type for generating map
    
    * adding unit_type for generating mapV2
    
    * reverse the coordinate of pathfinding
    
    * changing AStar class to finding_path function
    
    * changing AStar class to finding_path function+
    
    * poetry update
    
    * pathfinding V2

---
+++ tests/test_pathfinding.py
@@ -0,0 +1,65 @@
+from COE.logic.path_finding import find_move
+from COE.map.map import Map
+from COE.map.enum.map_sizes import MapSizes
+from COE.map.enum.map_types import MapTypes
+from COE.map.enum.resources_rarity import ResourcesRarity
+from COE.map.enum.cell_types import CellTypes
+from COE.contents.unit.enum.unit_types import UnitTypes
+
+
+def test_path_finding():
+
+    map_1 = Map(MapSizes.TINY, MapTypes.CONTINENTAL, ResourcesRarity.HIGH)
+
+    # for testing only
+    map_1.change_cell(0, 3, CellTypes.WATER)
+    map_1.change_cell(1, 1, CellTypes.WATER)
+    map_1.change_cell(1, 2, CellTypes.WATER)
+    map_1.change_cell(1, 3, CellTypes.WATER)
+    map_1.change_cell(2, 1, CellTypes.WATER)
+    map_1.change_cell(2, 2, CellTypes.WATER)
+    map_1.change_cell(2, 3, CellTypes.WATER)
+    map_1.change_cell(3, 1, CellTypes.WATER)
+    map_1.change_cell(3, 2, CellTypes.WATER)
+    map_1.change_cell(3, 3, CellTypes.WATER)
+
+    trans_map_for_ground_unit = map_1.transform_for_unit(UnitTypes.GROUND)
+    trans_map_for_navy_unit = map_1.transform_for_unit(UnitTypes.NAVY)
+
+    assert find_move(trans_map_for_ground_unit, (0, 0), (2, 2), UnitTypes.GROUND) == []
+    assert find_move(trans_map_for_ground_unit, (0, 0), (4, 2), UnitTypes.GROUND) == [
+        (1, 0),
+        (2, 0),
+        (3, 0),
+        (4, 1),
+        (4, 2),
+    ]
+    assert find_move(trans_map_for_ground_unit, (0, 0), (0, 4), UnitTypes.GROUND) == [
+        (1, 0),
+        (2, 0),
+        (3, 0),
+        (4, 1),
+        (4, 2),
+        (4, 3),
+        (3, 4),
+        (2, 4),
+        (1, 4),
+        (0, 4),
+    ]
+    assert find_move(trans_map_for_ground_unit, (0, 4), (0, 2), UnitTypes.GROUND) == [
+        (1, 4),
+        (2, 4),
+        (3, 4),
+        (4, 3),
+        (4, 2),
+        (4, 1),
+        (3, 0),
+        (2, 0),
+        (1, 0),
+        (0, 1),
+        (0, 2),
+    ]
+    assert find_move(trans_map_for_navy_unit, (1, 1), (3, 3), UnitTypes.NAVY) == [
+        (2, 2),
+        (3, 3),
+    ]
