0651e397d10a3a04a5b91fbc9cda0ad4ab581fbf
Quoc Thinh NGUYEN <quoc_thinh.nguyen@insa-cvl.fr>
2022-01-18 22:13:59 +0100

    fix commit

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -9,20 +9,20 @@ from COE.contents.unit.trade_boat import TradeBoat
 from COE.contents.unit.bowman import Bowman
 from COE.logic.Player import Player
 
+
 def test_unit():
     p1 = Player("Player 1", True, [], [], None, None)
     p2 = Player("Player 2", True, [], [], None, None)
-    v = Villager((2,2),p2)
-    b = Bowman((0,0),p1)
+    v = Villager((2, 2), p2)
+    b = Bowman((0, 0), p1)
     p1.units.append(b)
     p2.units.append(v)
 
-    assert p1.units[0].check_in_range(p2.units[0]) 
-    assert p2.units[0].check_in_range(p1.units[0]) == False
+    assert p1.units[0].check_in_range(p2.units[0])
+    assert not p2.units[0].check_in_range(p1.units[0])
     p1.units[0].attacked_entity = p2.units[0]
     p1.units[0].is_attacking = True
     p1.units[0].attack()
     assert p2.units[0].hp == 46
     p2.units[0].hp = 0
-    assert p2.units[0].die() 
-    
+    assert p2.units[0].die()
