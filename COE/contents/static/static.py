052c8e1bfadb831eea142070f929ff492ffe51da
Octave <octave.gassot_laffont@insa-cvl.fr>
2022-02-02 19:19:05 +0100

    Changing assets

--- COE/contents/static/static.py
+++ COE/contents/static/static.py
@@ -6,12 +6,14 @@ from COE.map.cell import Cell
 class Static:  # pragma: no cover
     def __init__(self) -> None:
         self.entities_pos_dict = {
-            "tree": (8, -50),
+            "tree": (-100, -150),
             "villager": (25, -50),
+            "axeman": (20, -40),
             "gold ore": (10, -8),
             "stone ore": (10, -8),
             "berry": (8, -27),
             "house": (-5, 10),
+            "towncenter": (-55, -10),
         }
         self.cells_size = Cell.get_pixel_cells_size()
         self.scaled_blocks = Cell.get_scaled_blocks()
